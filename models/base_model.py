527287e67f955e0861e203b0c704cd0f8e339b2b
mouadnait <moadnait6@gmail.com>
2023-10-10 13:03:44 +0100

    3. BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import uuid
+import datetime as dt
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = dt.datetime.now()
+        self.updated_at = dt.datetime.now()
+
+    def __str__(self):
+        return "[{}] (<{}>) <{}>".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        return self.updated_at
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+        
