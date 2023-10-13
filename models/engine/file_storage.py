5cb869c8993e1f6421e7076d2c3b0d471e78ffc6
mouadnait <moadnait6@gmail.com>
2023-10-13 23:57:37 +0100

    8. First User

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,8 +10,10 @@ class FileStorage:
     def classes(self):
         """Returns a dictionary of valid classes and their references."""
         from models.base_model import BaseModel
+        from models.user import User
 
-        classes = {"BaseModel": BaseModel}
+        classes = {"BaseModel": BaseModel,
+                   "User": User}
         return classes
 
     def all(self):
