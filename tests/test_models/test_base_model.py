dfb8da48d4910db5d03b2c89d7978f7812e8cfaf
mouadnait <moadnait6@gmail.com>
2023-10-15 21:35:32 +0100

    2. Unittests

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module tests the base class
+"""
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    """tests for the base class"""
+
+    pass
